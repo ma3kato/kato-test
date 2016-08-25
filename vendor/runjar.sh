42a9afd4784978014af9f33111acf81a5d315de0
ma3kato <czg00572@nifty.ne.jp>
2016-08-25 17:41:05 +0900

    first commit

---
+++ vendor/runjar.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+# Unit Test
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar $1
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/XlsxInfoManager.java,../testdata/FileUtility.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/XlsxInfoManager.java,../testdata/FileUtility.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/XlsxInfoManager.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/FileUtility.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/StringUtility.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/StringUtility2.java, ../testdata/StringUtility.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/FileUtility.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src/jp/co/nil/xlchecker/XlCheckerManager.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src/jp/co/nil/xlchecker/Global.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src/jp/co/nil/xlchecker/Resource.java
+
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/ModelRole.java
+
+# CheckAll Class
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src ../testdata/FileUtility.java
+
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/euc,../testdata/sjis,../testdata/utf,../testdata/sample
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/euc ../testdata/sjis ../testdata/utf,../testdata/sample
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/sample
+
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src/jp/co/nil/xlchecker/analyze/JavaCodeAnalyzer.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src/jp/co/nil/xlchecker/util/StringUtility.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src/jp/co/nil/xlchecker/XlCheckerManager.java
+# -Xmx256m
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/XlsxInfoManager.java,../testdata/Main.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/XlsxInfoManager.java,../testdata/src/jp/co/nil/util/Comparator.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/src/jp/co/nil/util/Comparator.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/XlsxInfoManager.java,../testdata/uploadPredicateItems.java
+
+# EUC-JP
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/XlsxInfoManager.java,../testdata/euc/XlCheckerManager.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/Main.java ../testdata/FileUtility2.java ../testdata/Global.java~
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/src/jp/co/nil/xlchecker/EnglishOnly.java
+## BINARY datas
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/BinaryFile.java ../testdata/src/jp/co/nil/xlchecker/EnglishOnly.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/single.java ../testdata/manual_ja.java
+
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/WindowsUTFFile.java ../testdata/src/jp/co/nil/xlchecker/EnglishOnly.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/sample/analyzer
+
+# tool sources
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/nirvana/kaosplugin
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/nirvana/nirvana
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/nirvana/wbsplugin
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/poi/poi-3.10-FINAL/src
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/poi/poi-3.10-FINAL/src/java/org/apache/poi/poifs/crypt
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/poi/poi-3.10-FINAL/src/java/org/apache/poi/ddf/EscherDump.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/srcStandalone
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/srcStandalone/com/toyota_itc/documentchecker/DocCheckerManager.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/srcStandalone/com/toyota_itc/documentchecker/gui/QuestionPanel.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/srcStandalone/com/toyota_itc/documentchecker/io/MSWordWrapper.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/srcStandalone/com/toyota_itc/documentchecker/io/server/ServerWriter.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/srcStandalone/com/toyota_itc/documentchecker/tool/TreeModelMaker.java
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/MovieMaker/src
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/jresrc/javax/swing/colorchooser
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/jresrc/javax/swing/text
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/jresrc/java/lang
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/jresrc/javax/swing/text/html/StyleSheet.java
+
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/DocumentChecker2.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L -d ../testdata3,../testdata2 ../testdata/Main.java ../testdata2/Main.java ../testdata/StringUtility.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L -d ../testdata/with,Dir -d "../testdata/確認よう フォルダ"
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L -d ../testdata/euc -d "../testdata/確認よう フォルダ"
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -d ../testdata/euc main.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -d ../testdata/euc main.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -d ../testdata/euc StringUtilityEUCWin.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -d ../testdata/euc -d ../testdata/src StringUtilityEUCWin.java Global.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar ../testdata/euc/StringUtilityEUCWin.java Global.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L -d ../testdata3
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/final/CheckIFb.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/final/test1/CheckT.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L /c/home/test/test1/CheckIF.java /c/home/test/test1/CheckException.java /c/home/test/test2/CheckT.java
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L -d ../testdata/jresrc
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -L ../testdata/SynthSpinnerUI.java
+
+# for VB test
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -l b -d ../testdata/vbsrc
+
+# for VB test
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.2.jar -l c -d ../testdata/vcsrc/QxOrm -L get_base_class.h
+
+# for public Ver0.3
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.3.1.jar "$@"
+# for public Ver0.3.1
+#java -Xmx768m -Dmyapp.debug="false" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.3.1.jar "$@"
+# for public Ver0.4.0
+#java -Xmx768m -Dmyapp.debug="true" -Dspecchecker.home=. -Dapple.laf.useScreenMenuBar=true -jar xlchecker0.4.jar "$@"
+# for public Ver0.5.0
+java -Xmx768m -Dmyapp.debug="true" -Dcardionspec.home=. -Dapple.laf.useScreenMenuBar=true -jar cardionspec0.1.jar "$@"
